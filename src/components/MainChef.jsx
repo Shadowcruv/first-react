83bf40a45c3442ef0d8c6c8de00d29f382fb824c
Miracle Ihediwa <miracleihediwa619@gmail.com>
2025-03-27 03:41:44 +0100

    practising useStates with Arrays. using 2 useStates

--- src/components/MainChef.jsx
+++ src/components/MainChef.jsx
@@ -6,6 +6,8 @@ export default function MainChef(){
 
   const [ingredientList, setIngredientList] = React.useState([])
 
+  const [count, setCount] = React.useState(0)
+
 
   const ingredients = ["Pepper", "CrayFish", "Maggi"]
   
@@ -20,16 +22,33 @@ export default function MainChef(){
   // )
 
   function handleSubmit(event){
-    //to preventing reloading of the page when the button in the form is clicked
     event.preventDefault()
-    //to get the text in the input box in the form
-    const formData = new FormData(event.currentTarget);
-    const newIngredient = formData.get("ingredient");
+    setCount(count => {
 
-    setIngredientList(prevList => [...prevList, newIngredient])
+      setIngredientList( prevList => {
+        console.log(count)
+        return [...prevList, ingredients[count]]
+      })
+      
+      
+      return count + 1
 
-    console.log(newIngredient);
+    
+    })
+    
   }
+
+  // function handleSubmit(event){
+  //   //to preventing reloading of the page when the button in the form is clicked
+  //   event.preventDefault()
+  //   //to get the text in the input box in the form
+  //   const formData = new FormData(event.currentTarget);
+  //   const newIngredient = formData.get("ingredient");
+
+  //   setIngredientList(prevList => [...prevList, newIngredient])
+
+  //   console.log(newIngredient);
+  // }
  
   return (
     <>
